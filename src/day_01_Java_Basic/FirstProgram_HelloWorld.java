95e8966a22eb1a8afb4ecef17cc09f2d42e08f97
mona538 <monikapawar538@gmail.com>
2025-06-06 14:10:24 +0530

    Hello World program Updated

--- src/day_01_Java_Basic/FirstProgram_HelloWorld.java
+++ src/day_01_Java_Basic/FirstProgram_HelloWorld.java
@@ -5,5 +5,7 @@ public class FirstProgram_HelloWorld {
     public static void main (String [] args) {
 
         System.out.println("Hello Beautiful World");
+
+        System.out.println("India is my country");
     }
 }
