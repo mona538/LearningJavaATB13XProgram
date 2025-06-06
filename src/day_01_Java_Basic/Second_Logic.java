eda68e41f75ed44a24cdb752dc6af92397ce9f98
mona538 <monikapawar538@gmail.com>
2025-06-06 21:15:01 +0530

    Add new code

--- src/day_01_Java_Basic/Second_Logic.java
+++ src/day_01_Java_Basic/Second_Logic.java
@@ -1,9 +1,12 @@
- package day_01_Java_Basic;
+
+package day_01_Java_Basic;
 
 public class Second_Logic {
 
     public static void main (String [] args) {
 
         System.out.println("Additon of " + "10 and 9.2" + "=" + 10 + 9.2);
+        System.out.println(2 + 2);
+
     }
 }
