ccf1b778368b6388d927f2ddb03bf7694102e18b
mona538 <monikapawar538@gmail.com>
2025-06-06 14:02:15 +0530

    Hello World program

---
+++ src/day_01_Java_Basic/FirstProgram_HelloWorld.java
@@ -0,0 +1,9 @@
+package day_01_Java_Basic;
+
+public class FirstProgram_HelloWorld {
+
+    public static void main (String [] args) {
+
+        System.out.println("Hello Beautiful World");
+    }
+}
