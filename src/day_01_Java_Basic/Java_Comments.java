7f19b25e55356fcfb1d5c566c9de765b3e16a932
mona538 <monikapawar538@gmail.com>
2025-06-09 23:04:54 +0530

    Add new code addition comment

---
+++ src/day_01_Java_Basic/Java_Comments.java
@@ -0,0 +1,15 @@
+package day_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Java_Comments {
+
+
+    public static void main(String[] args) {
+        System.out.println("Comment is added in Java");
+
+        System.out.println("Hi");
+        System.out.println("bye");
+//        System.out.println("pilo chai");
+    }
+}
